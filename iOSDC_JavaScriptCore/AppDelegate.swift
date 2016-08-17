b2db367e29e7a1b41a545e20e831f8e426cc7804
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-08-17 17:16:44 +0900

    Initial Commit

---
+++ iOSDC_JavaScriptCore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iOSDC_JavaScriptCore
+//
+//  Created by Tomohiro Kumagai on 8/17/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iOSDC_JavaScriptCore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOSDC_JavaScriptCore
+//
+//  Created by Tomohiro Kumagai on 8/17/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
